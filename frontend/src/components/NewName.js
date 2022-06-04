cc994725b7d2e08959671e3f15e72af3d2cbced1
Swathi P <bhatswathip@gmail.com>
2022-06-04 14:06:20 +0530

    uitrial

--- frontend/src/components/NewName.js
+++ frontend/src/components/NewName.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import { white } from 'tailwindcss/colors';
 
 class NewName extends Component {
     render() {
         return (
-            <div>
-                <form onSubmit={this.props.handleSubmit}>
+              <form onSubmit={this.props.handleSubmit}>
                     <input
                         type="text"
                         name="NewName"
@@ -14,9 +14,27 @@ class NewName extends Component {
                         autoFocus
                         autoComplete='off'
                     />
-                    <button type="submit">Add</button>
+
+                    
+                    <input
+                      type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Email"
+                      name="email"
+                    />
+                  
+
+                 
+                    <div className="text-center mt-6">
+                    <button
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="submit"
+                    >
+                      Add
+                    </button>
+                  </div>
                 </form>
-            </div>
+           
         )
     }
 }
